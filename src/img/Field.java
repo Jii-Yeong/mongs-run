5ffd4f103e5cc9e920b1184474e668664ae9eea2
GG <8shot@naver.com>
2021-03-16 11:36:21 +0900

    땅 채워넣음

---
+++ src/img/Field.java
@@ -0,0 +1,18 @@
+package img;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class Field extends JPanel {
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	/**
+	 * Create the panel.
+	 */
+	public Field() {
+		setBackground(new Color(0, 0, 0));
+	}
+}
