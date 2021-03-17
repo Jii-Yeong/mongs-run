f0787cf60034c0e01f7798406c1a9e05d3da2900
user <qqs2615@naver.com>
2021-03-17 15:21:53 +0900

    시작버튼 눌렀을 때 게임진행, 랭킹 점수 연동시킴

--- src/panel/PlayPanel.java
+++ src/panel/PlayPanel.java
@@ -27,6 +27,9 @@ import main.MainFrame;
 import score.ScorePanel;
 import panel.BackgroundPanel;
 import physical.Physical;
+import ranking.RankData;
+import ranking.RankPanel;
+import result.ResultPanel;
 
 public class PlayPanel extends JPanel {
 	BackgroundPanel background = new BackgroundPanel();
@@ -46,9 +49,14 @@ public class PlayPanel extends JPanel {
 	static int red = new Color(237, 28, 36).getRGB();
 	static int yellow = new Color(255, 242, 0).getRGB();
 
+	private RankPanel rankPanel;
+	private MainFrame frame;
+
 	
 	public PlayPanel(MainFrame frame) {
 		field = new Field();
+		this.frame = frame;
+		
 		JButton lifeUp = new JButton("Up");
 		lifeUp.setBounds(0, 300, 100, 100);
 		lifeUp.addActionListener(new ActionListener() {
@@ -86,6 +94,8 @@ public class PlayPanel extends JPanel {
 		btn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				rankPanel = new RankPanel(frame.getStartPanel() , scorePanel, frame);
+				frame.getContentPane().add("rank", rankPanel);
 				frame.changeRankPanel();
 			}
 		});
