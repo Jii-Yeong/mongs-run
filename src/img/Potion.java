ab28f6ae010c58ed63b2e3c058725479e3edd745
GG <8shot@naver.com>
2021-03-19 10:14:46 +0900

    getGreen으로 체력회복수단 만들고 1스테이지 오브젝트 배치함

---
+++ src/img/Potion.java
@@ -0,0 +1,16 @@
+package img;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class Potion extends JPanel {
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+
+	public Potion() {
+		setBackground(new Color(34, 177, 76));
+	}
+}
