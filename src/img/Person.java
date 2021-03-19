c44fe55f23bd7ff1ac81d608de8f3fd1a08abec8
hyeleeKim <hyek0226@gmail.com>
2021-03-19 15:47:38 +0900

    선택한 캐릭터 play 패널에 연결 기능 추가, 병아리(chick) 캐릭터 추가

--- src/img/Person.java
+++ src/img/Person.java
@@ -10,10 +10,38 @@ import javax.swing.JPanel;
 public class Person extends JPanel {
 	private Image im;
 	ImageIcon img;
-	public Person() {
-		img = new ImageIcon(".\\img\\Person.gif");
-		im = img.getImage();
-//		setBounds(0, 0, 200, 400);
+	
+	// 달리는 캐릭터
+	public Image person_run = new ImageIcon(".\\img\\Person.gif").getImage();
+	public Image chick_run = new ImageIcon(".\\img\\chick_run.gif").getImage();
+	
+	// 점프 캐릭터
+	public Image person_jump = new ImageIcon(".\\img\\person_jumping.png").getImage();
+	public Image chick_jump = new ImageIcon(".\\img\\chick_jump.png").getImage();
+	
+	// 슬라이딩 캐릭터
+	public Image person_sliding = new ImageIcon(".\\img\\Person_sliding.png").getImage();
+	public Image chick_sliding = new ImageIcon(".\\img\\chick_jump.png").getImage();
+	
+	
+	public Image run;
+	public Image slide;
+	public Image jump;
+	
+	
+	public Person(int selectedNum) {
+		
+		switch (selectedNum) {
+		case 1:
+			setIm(person_run);
+			break;
+		case 2:
+			setIm(chick_run);
+			break;
+		}
+		switchRun(selectedNum);
+		switchSlid(selectedNum);
+		switchJump(selectedNum);
 	}
 	
 	@Override
@@ -37,6 +65,59 @@ public class Person extends JPanel {
 	public void setIm(Image im) {
 		this.im = im;
 	}
-	
+
+	public Image getSlide() {
+		return slide;
+	}
+
+	public void setSlide(Image slide) {
+		this.slide = slide;
+	}
+	public Image getRun() {
+		return run;
+	}
+
+	public void setRun(Image run) {
+		this.run = run;
+	}
+
+	public Image getJump() {
+		return jump;
+	}
+
+	public void setJump(Image jump) {
+		this.jump = jump;
+	}
+
+	public void switchSlid(int selectedNum) {
+		switch (selectedNum) {
+		case 1:
+			setSlide(person_sliding);
+			break;
+		case 2:
+			setSlide(chick_sliding);
+			break;
+		}
+	}
+	public void switchRun(int selectedNum) {
+		switch (selectedNum) {
+		case 1:
+			setRun(person_run);
+			break;
+		case 2:
+			setRun(chick_run);
+			break;
+		}
+	}
+	public void switchJump(int selectedNum) {
+		switch (selectedNum) {
+		case 1:
+			setJump(person_jump);
+			break;
+		case 2:
+			setJump(chick_jump);
+			break;
+		}
+	}
 	
 }
